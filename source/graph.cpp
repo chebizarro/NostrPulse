aacd28d875027aa31e627aa9b54e6dd9ecca2ee2
Chris Daley <chebizarro@gmail.com>
2024-12-22 11:17:45 -0800

    minor edit

--- source/graph.cpp
+++ source/graph.cpp
@@ -3,23 +3,21 @@
 using namespace nostr_pulse;
 
 int Graph::getOrCreateNodeId(const std::string& pubkey) {
-    auto it = pubkeyToNodeId.find(pubkey);
-    if (it != pubkeyToNodeId.end()) {
-        return it->second;
-    }
-    int newId = nextNodeId++;
-    nodes.emplace_back(pubkey, newId);
-    pubkeyToNodeId[pubkey] = newId;
-    return newId;
+  auto it = pubkeyToNodeId.find(pubkey);
+  if (it != pubkeyToNodeId.end()) {
+    return it->second;
+  }
+  int newId = nextNodeId++;
+  nodes.emplace_back(pubkey, newId);
+  pubkeyToNodeId[pubkey] = newId;
+  return newId;
 }
 
-Node& Graph::getNodeById(int nodeId) {
-    return nodes[nodeId];
-}
+Node& Graph::getNodeById(int nodeId) { return nodes[nodeId]; }
 
 void Graph::addEdge(const std::string& srcPubkey, const std::string& dstPubkey) {
-    int srcId = getOrCreateNodeId(srcPubkey);
-    int dstId = getOrCreateNodeId(dstPubkey);
+  int srcId = getOrCreateNodeId(srcPubkey);
+  int dstId = getOrCreateNodeId(dstPubkey);
 
-    edges.emplace_back(srcId, dstId);
+  edges.emplace_back(srcId, dstId);
 }
